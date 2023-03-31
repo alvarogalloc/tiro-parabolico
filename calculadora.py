44ddef7c6f5d1b16dae0d38bbe3f062abf6bf84a
mateosanzep <mateosanzep.gmail.com>
2023-03-30 22:47:31 -0600

    Pusimos los text areas y el boton pero con grid

--- calculadora.py
+++ calculadora.py
@@ -1,15 +1,50 @@
-import tkinter as tk
+import sys
 
-root = tk.Tk()
-root.title("calculadora pituda")
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
 
- root.attributes("-fullscreen", True)
-root.geometry("400x400+200+200")
-root.resizable(False, False)
+    QLineEdit,
+    QMainWindow,
 
-frm = tk.Frame(root, padding=10)
+    QPushButton,
+    QLabel,
+    QGridLayout,
+    QWidget,
+)
 
-button = tk.Button(root, text="Hola")
-button.pack()
 
-root.mainloop()
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Widgets App")
+
+        layout = QGridLayout()
+
+        action_button = QPushButton("calcular")
+        input_names = {
+            0: "Variable t",
+            1: "Gravedad (g)",
+            2:"Altura Inicial (h0)",
+            3:"Altura Final (hf)",
+            4:"Constante del Resorte (k)",
+        }
+        for n in range(5):
+            layout.addWidget(QLabel(f"ingresa valor {input_names[n]}"), n, 0)
+            layout.addWidget(QLineEdit(), n, 1)
+        layout.addWidget(action_button, 5, 0, 1, 2)
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+        self.setCentralWidget(widget)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+
+app.exec()
