9092d76c2fedf51ef2cf7bf90af83a62498ac7c3
mateosanzep <mateosanzep.gmail.com>
2023-10-02 20:04:33 -0600

    no sé que cambie

--- draft_diseño.py
+++ draft_diseño.py
@@ -10,7 +10,6 @@ from PyQt6.QtWidgets import (
     QCheckBox,
     QPushButton,
     QLineEdit,
-    QVBoxLayout,
 )
 
 basedir = os.path.dirname(__file__)
@@ -29,39 +28,37 @@ class VentanaPrincipal(QMainWindow):
         layout_principal.setColumnStretch(1, 2)
         layout_principal.setColumnStretch(2, 3)
         lado_izquierdo = QGridLayout()
-        lado_derecho = QVBoxLayout()
+        lado_derecho = QGridLayout()
         
-        boton_ayuda = QPushButton("Ayuda", self)
+        boton_ayuda = QPushButton("Ayuda")
         boton_reset = QPushButton("Resetear")
         boton_calcular = QPushButton("Calcular")
-        self.checkbox = QCheckBox()
+        self.checkbox = QCheckBox("Auto")
         label_entrada = QLabel("Valores de Entrada")
         label_salida = QLabel("Valores de Salida")
         output_names = [
-            "Angulo (θ)",
+            "Ángulo (θ)",
             "Compresión del resorte (Xc)",
         ]
         entrada_nombres = [
-            "Masa Balon",
+            "Masa Balón",
             "Gravedad",
             "Constante",
             "Altura Final",
-            "Altura Incial",
+            "Altura Inicial",
             "Distancia Horizontal",
-            "Obstaculo"
+            "Obstáculo"
         ]
         
+        label_entrada.setObjectName("label_entrada")
+        label_salida.setObjectName("label_salida")
+        
         layout_principal.addWidget(label_entrada, 0, 1)
-        
-        layout_principal.addWidget(label_salida, 0,2)
-        
+        layout_principal.addWidget(label_salida, 0, 2)
         layout_principal.addWidget(boton_ayuda, 0, 0)
 
         lado_izquierdo.addWidget(self.checkbox, 9, 1)
-        self.checkbox.setText("Auto")
-        
         lado_izquierdo.addWidget(boton_reset, 8, 1)
-        
         lado_izquierdo.addWidget(boton_calcular, 8, 2)
 
         layout_principal.addLayout(lado_izquierdo, 1, 1)
@@ -73,19 +70,17 @@ class VentanaPrincipal(QMainWindow):
         for element in entrada_nombres:
             grid_row += 1
             lado_izquierdo.addWidget(QLabel(f"{element}"), grid_row, 1)
-            entrada = QLineEdit()  # crea un widget QLineEdit
-            lado_izquierdo.addWidget(entrada, grid_row, 2)  # agrega el widget al layout
-        
+            entrada = QLineEdit()
+            lado_izquierdo.addWidget(entrada, grid_row, 2)
 
         for m in range(len(output_names)):
-            titulos = QLabel((output_names[m]))
+            titulos = QLabel(output_names[m])
             lado_derecho.addWidget(titulos)
             titulos.setAlignment(Qt.AlignmentFlag.AlignHCenter)
             salida = QLabel("3.28929")
             salida.setAlignment(Qt.AlignmentFlag.AlignHCenter)
             lado_derecho.addWidget(salida)
             
-        
     def close_on_esc(self, event):
         if event.key() == Qt.Key.Key_Escape:
             self.close()
@@ -98,8 +93,3 @@ if __name__ == "__main__":
     window.show()
 
     app.exec()
-
-
-
-
-
